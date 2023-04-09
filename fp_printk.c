e18e56b1e31d106b8ca06814fda8c12c92774e84
simoes <trintaeoitogc@gmail.com>
2023-04-09 10:12:27 -0300

    tmp: comment temporary

--- fp_printk.c
+++ fp_printk.c
@@ -7,26 +7,26 @@ MODULE_DESCRIPTION("This lib will show float-point in printk()");
 MODULE_LICENSE("MIT");
 MODULE_VERSION("0.1");
 
-static void __init fp_printk_init(float number) 
-{
-    fp_printk();
-}
-
+//static void __init fp_printk_init(float number) 
+//{
+//    fp_printk();
+//}
+//
 static char* fp_printk(float number) 
-{
-
-}
-
-static void __exit fp_printk_exit(void) 
 {
     size_t number_length = snprintf(NULL, 0, "%f", number);
     char number_in_str = malloc(number_length);
     snprinf(number_in_str, number_length+1, "%f", number);
 
     return buff;
+
 }
 
-module_init(fp_printk);
-module_exit();
+//static void __exit fp_printk_exit(void) 
+//{
+//}
+
+//module_init(fp_printk);
+//module_exit();
 
 EXPORT_SYMBOL(fp_printk);
