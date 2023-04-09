9abc3d4dba958c510346187c86304dfaf30a0288
simoes <trintaeoitogc@gmail.com>
2023-04-09 10:03:08 -0300

    refactor: divided in two new functions

--- fp_printk.c
+++ fp_printk.c
@@ -7,9 +7,26 @@ MODULE_DESCRIPTION("This lib will show float-point in printk()");
 MODULE_LICENSE("MIT");
 MODULE_VERSION("0.1");
 
-static char* __init fp_printk(float number) 
+static void __init fp_printk_init(float number) 
 {
 
 }
+
+static void __exit fp_printk_exit(void) 
+{
+    size_t number_length = snprintf(NULL, 0, "%f", number);
+    char number_in_str = malloc(number_length);
+    snprinf(number_in_str, number_length+1, "%f", number);
+
+    return buff;
+}
+
+static char* fp_printk(float number) 
+{
+
+}
+
+module_init(fp_printk);
+module_exit();
 
 EXPORT_SYMBOL(fp_printk);
