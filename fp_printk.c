9bb87279ba56c0a70aeed41d7cfe1566a2c888dc
simoes <trintaeoitogc@gmail.com>
2023-04-09 10:02:52 -0300

    refactor: the license is only MIT

--- fp_printk.c
+++ fp_printk.c
@@ -4,7 +4,7 @@
 
 MODULE_AUTHOR("Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>");
 MODULE_DESCRIPTION("This lib will show float-point in printk()");
-MODULE_LICENSE("Dual MIT/GPL");
+MODULE_LICENSE("MIT");
 MODULE_VERSION("0.1");
 
 static char* __init fp_printk(float number) 
