8d4cb23f03b58dc569caa0e1fb49f1c6be944834
simoes <trintaeoitogc@gmail.com>
2023-04-10 22:26:55 -0300

    feat: fp_printk function incremented

--- fp_printk.c
+++ fp_printk.c
@@ -14,12 +14,26 @@ MODULE_VERSION("0.1");
 //
 static char* fp_printk(float number) 
 {
-    size_t number_length = snprintf(NULL, 0, "%f", number);
-    char number_in_str = malloc(number_length);
-    snprinf(number_in_str, number_length+1, "%f", number);
+    number += 0.005;
+    char result[100];
 
-    return buff;
+    int d_val = number, 
+        dec = (int) (number * 100) % 100, 
+        i=3;
 
+    memset(result, 0, 100);
+    result[0] = (dec % 10) + '0';
+    result[1] = (dec / 10) + '0';
+    result[2] = '.';
+
+    while(d_val > 0) 
+    {
+        result[i] = (d_val % 10) + '0';
+        d_val /= 10;
+        i++;
+    }
+
+    return result;
 }
 
 //static void __exit fp_printk_exit(void) 
