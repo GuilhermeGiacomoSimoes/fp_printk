4df98482b97457df71529cf8b0f19cc837e273da
simoes <trintaeoitogc@gmail.com>
2023-04-21 13:45:08 -0300

    style: indent

--- fp_printk.c
+++ fp_printk.c
@@ -9,12 +9,12 @@ MODULE_VERSION("0.4.1");
 
 void fp_printk(int number, int decimal_places)
 {
-	static char buffer[32] = {0};
+	static char buffer[32] = { 0 };
 	int buf_index = 30;
 	int count_decimal_place = 0;
 	int point_include = 0;
 
-	for(; number && buf_index; --buf_index, number /= 10) {
+	for (; number && buf_index; --buf_index, number /= 10) {
 		count_decimal_place++;
 		if(!point && count_decimal_place > decimal_places) {
 			buffer[i] = '.';
