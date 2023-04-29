e7730492a6ba325eecde0785d0c5be869b764a43
simoes <trintaeoitogc@gmail.com>
2023-04-28 23:46:02 -0300

    temp: temporary in fp_printk.c

--- fp_printk.c
+++ fp_printk.c
@@ -14,8 +14,23 @@ MODULE_VERSION("0.4.1");
 
 void fp_printk(int number, int decimal_places, char* destination)
 {
-	char* str_result = int_to_fp_str(number, decimal_places);
-	strcpy(destination, str_result);
+	static char buffer[32] = { 0 };
+	int buf_index = 30;
+	int count_decimal_place = 0;
+	int point_include = 0;
+
+	for (; number && buf_index; --buf_index, number /= 10) {
+		count_decimal_place++;
+		if (!point_include && count_decimal_place > decimal_places) {
+			buffer[buf_index] = '.';
+			buf_index--;
+			point_include = 1;
+		}
+
+		buffer[buf_index] = "0123456789"[number % 10];
+	}
+
+	strcpy(destination, &buffer[buf_index + 1]);
 }
 
 static int __init fp_printk_init(void)
--- service.c
+++ service.c
@@ -1,21 +1,3 @@
 #include "service.h"
 
 char* int_to_fp_str(int number, int decimal_places) {
-	static char buffer[32] = { 0 };
-	int buf_index = 30;
-	int count_decimal_place = 0;
-	int point_include = 0;
-
-	for (; number && buf_index; --buf_index, number /= 10) {
-		count_decimal_place++;
-		if (!point_include && count_decimal_place > decimal_places) {
-			buffer[buf_index] = '.';
-			buf_index--;
-			point_include = 1;
-		}
-
-		buffer[buf_index] = "0123456789"[number % 10];
-	}
-
-	return &buffer[buf_index + 1];
-}
