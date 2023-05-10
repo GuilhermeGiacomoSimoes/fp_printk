312457691742f933170c1636dfe3471f393b304d
simoes <trintaeoitogc@gmail.com>
2023-05-09 22:31:55 -0300

    feat: add the test case struct and suit test in the module test

--- fp_printk_test.c
+++ fp_printk_test.c
@@ -56,3 +56,23 @@ static void should_return_ERROR_if_parse_destination_NULL(struct kunit *test)
     KUNIT_FAIL(test, "0", fp_printk(12, 0, NULL));
     kfree(destination);
 }
+
+static struct kunit_case fp_printk_test_case[] = {
+    KUNIT_CASE(should_return_ERROR_if_parse_destination_NULL),
+    KUNIT_CASE(should_return_ERROR_if_parse_number_NULL),
+    KUNIT_CASE(should_return_12_without_errors_and_float_point),
+    KUNIT_CASE(should_return_0_without_errors),
+    KUNIT_CASE(should_return_2_without_errors_and_float_point),
+    KUNIT_CASE(should_return_234_without_errors),
+    KUNIT_CASE(should_return_2345_without_errors),
+    {/* sentinel */}
+};
+
+static struct kunit_suit fp_printk_test_suite = {
+    .name = "fp_printk_test",
+    .test_cases = fp_printk_test_case
+};
+kunit_test_suite(fp_printk_test_suite);
+
+MODULE_AUTHOR("Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>");
+MODULE_LICENSE("GPL");
