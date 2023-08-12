3d21ba3041612eaf6785bd0579ad0c8135e026a4
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2023-08-11 23:25:56 -0300

    test: refactor tests

--- int_to_fp_str_test.c
+++ int_to_fp_str_test.c
@@ -5,60 +5,45 @@
 
 static void should_return_2345_without_errors(struct kunit *test)
 {
-	char *destination = kmalloc(sizeof("23.45", GFP_KERNEL));
-	fp_printk(2345, 2, destination);
+	char *destination = int_to_fp_str(2345, 2);
 	KUNIT_EXPECT_EQ(test, "23.45", destination);
 	kfree(destination);
 }
 
 static void should_return_234_without_errors(struct kunit *test)
 {
-	char *destination = kmalloc(sizeof("2.45", GFP_KERNEL));
-	fp_printk(245, 2, destination);
+	char *destination = int_to_fp_str(245, 2);
 	KUNIT_EXPECT_EQ(test, "2.45", destination);
 	kfree(destination);
 }
 
 static void should_return_2_without_errors_and_float_point(struct kunit *test)
 {
-	char *destination = kmalloc(sizeof("2", GFP_KERNEL));
-	fp_printk(2, 0, destination);
+	char *destination = int_to_fp_str(2, 0);
 	KUNIT_EXPECT_EQ(test, "2", destination);
 	kfree(destination);
 }
 
 static void should_return_0_without_errors(struct kunit *test)
 {
-	char *destination = kmalloc(sizeof("0", GFP_KERNEL));
-	fp_printk(0, 0, destination);
+	char *destination = int_to_fp_str(0, 0);
 	KUNIT_EXPECT_EQ(test, "2", destination);
 	kfree(destination);
 }
 
 static void should_return_12_without_errors_and_float_point(struct kunit *test)
 {
-	char *destination = kmalloc(sizeof("12", GFP_KERNEL));
-	fp_printk(12, 0, destination);
+	char *destination = int_to_fp_str(12, 0);
 	KUNIT_EXPECT_EQ(test, "2", destination);
 	kfree(destination);
 }
 
 static void should_return_ERROR_if_parse_number_NULL(struct kunit *test)
 {
-	char *destination = kmalloc(sizeof("12", GFP_KERNEL));
-	KUNIT_FAIL(test, "2", fp_printk(NULL, 0, destination));
-	kfree(destination);
+	KUNIT_FAIL(test, "2", int_to_fp_str(NULL, 0));
 }
 
-static void should_return_ERROR_if_parse_destination_NULL(struct kunit *test)
-{
-	char *destination = kmalloc(sizeof("12", GFP_KERNEL));
-	KUNIT_FAIL(test, "0", fp_printk(12, 0, NULL));
-	kfree(destination);
-}
-
-static struct kunit_case fp_printk_test_case[] = {
-	KUNIT_CASE(should_return_ERROR_if_parse_destination_NULL),
+static struct kunit_case int_to_fp_str_test_case[] = {
 	KUNIT_CASE(should_return_ERROR_if_parse_number_NULL),
 	KUNIT_CASE(should_return_12_without_errors_and_float_point),
 	KUNIT_CASE(should_return_0_without_errors),
@@ -68,12 +53,12 @@ static struct kunit_case fp_printk_test_case[] = {
 	{ /* sentinel */ }
 };
 
-static struct kunit_suit fp_printk_test_suite = {
-	.name = "fp_printk_test",
-	.test_cases = fp_printk_test_case
+static struct kunit_suite int_to_fp_str_test = {
+	.name = "int_to_fp_str_test",
+	.test_cases = int_to_fp_str_test_case
 };
 
-kunit_test_suite(fp_printk_test_suite);
+kunit_test_suite(int_to_fp_str_test);
 
 MODULE_AUTHOR("Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>");
 MODULE_LICENSE("GPL");
